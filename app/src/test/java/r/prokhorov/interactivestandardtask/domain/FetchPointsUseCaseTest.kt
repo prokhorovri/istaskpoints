892b75d3064044f29458489740646812480002d6
Roman <prokhorov.r.i@gmail.com>
2023-05-26 19:48:16 +0300

    Added tests for FetchPointsUseCase

---
+++ app/src/test/java/r/prokhorov/interactivestandardtask/domain/FetchPointsUseCaseTest.kt
@@ -0,0 +1,47 @@
+package r.prokhorov.interactivestandardtask.domain
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import r.prokhorov.interactivestandardtask.data.FakePointsRepository
+import r.prokhorov.interactivestandardtask.domain.common.Result
+
+class FetchPointsUseCaseTest {
+
+    private lateinit var fetchPointsUseCase: FetchPointsUseCase
+    private lateinit var fakePointsRepository: FakePointsRepository
+
+    @Before
+    fun setUp() {
+        fakePointsRepository = FakePointsRepository()
+        fetchPointsUseCase = FetchPointsUseCase(FakePointsRepository())
+    }
+
+    @Test
+    fun `Fetch points with valid count`() = runBlocking {
+        val result = fetchPointsUseCase(500).first()
+        assertThat(result).isInstanceOf(Result.Success::class.java)
+    }
+
+    @Test
+    fun `Fetch points with 0`() = runBlocking {
+        val result = fetchPointsUseCase(0).first()
+        assertThat(result).isInstanceOf(Result.Failure::class.java)
+    }
+
+    @Test
+    fun `Fetch points with 1001`() = runBlocking {
+        val result = fetchPointsUseCase(1001).first()
+        assertThat(result).isInstanceOf(Result.Failure::class.java)
+    }
+
+    @Test
+    fun `Fetch points with unexpected server error`() = runBlocking {
+        fakePointsRepository.interruptUnexpectedly = true
+        val result = fetchPointsUseCase(1001).first()
+        assertThat(result).isInstanceOf(Result.Failure::class.java)
+    }
+
+}
